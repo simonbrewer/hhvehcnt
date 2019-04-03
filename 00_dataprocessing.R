baefb22deb780b6ea69f7036cd75d4e4563fb291
Simon Brewer <brewersi@gmail.com>
2019-04-03 16:06:06 -0600

    Modified preprocessing file for new data set

--- 00_dataprocessing.R
+++ 00_dataprocessing.R
@@ -1,16 +1,31 @@
 require(dplyr)
 
-dat = read.csv("Final.csv")
+## GLM model
+# vo.glmer <- glmer(veh ~ hhsize + wrkcount + lnhhfaminc + hh_race + 
+#                     lnactden + jobpop+ transitden + intden + 
+#                     pct4way + lnpopden_county + uber_year + taxi + 
+#                     rideshare + gasprice + (1|ct_new) + (1|county_new), 
+#                   data=data, family=poisson(),nAGQ=0, 
+#                   control = glmerControl(optimizer = "nloptwrap", calc.derivs = FALSE))
+
+dat = read.csv("Final_R.csv")
 str(dat)
 head(dat)
 
 dat2 = dat[,-c(1,2,3,4,5,6,7,8,9)]
-dat3 = dat2 %>% 
-  select(-tdaydate, -hhstate, -hh_cbsa, -scresp, -X_merge)
+dat3 = dat %>% 
+  select(veh, hhsize, wrkcount, lnhhfaminc, hh_race, lnactden,
+         jobpop, transitden, intden, pct4way, lnpopden_county, uber_year,
+         taxi, rideshare, gasprice, ct_new, county_new)
+
+dat3 = dat3 %>% filter(taxi > -7)
 
 dat3 = dat3[which(complete.cases(dat3)),]
+
 names(dat3)
 
-dat3.s = scale(dat3)
+save(dat3, file="data.RData")
 
-save(dat3.s, file="data.RData")
+# dat3.s = scale(dat3)
+# 
+# save(dat3.s, file="data.s.RData")
